d9e9bca01222ef818b40fa01b2c2eb92e733822a
Mahi-tech-p <leocodes01@gmail.com>
2026-08-06 21:25:17 +0530

    route configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 
 import authRoutes from "../modules/auth/auth.routes.js";
-import userRoutes from "../modules/users/user.routes.js"
-import parkRoutes from "../modules/parks/park.routes.js"
+import userRoutes from "../modules/users/user.routes.js";
+import parkRoutes from "../modules/parks/park.routes.js";
+import activityRoutes from "../modules/activities/activity.routes.js";
+
 const router = Router();
 
 router.use("/auth", authRoutes);
-router.use("/users", userRoutes)
+router.use("/users", userRoutes);
 
-router.use("/parks",parkRoutes)
+router.use("/parks", parkRoutes);
+
+router.use("/activities", activityRoutes);
 
 export default router;
